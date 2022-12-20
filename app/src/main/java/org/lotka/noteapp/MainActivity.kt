0bc2a29aa7ef193c23de9d1215d1067bf542c1bc
Mobin Jafari <Mobinjafari.dadegostar@gmail.com>
2022-12-20 14:43:46 +0330

    r

--- app/src/main/java/org/lotka/noteapp/MainActivity.kt
+++ app/src/main/java/org/lotka/noteapp/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setContentView(R.layout.activity_main)
+        //git setContentView(R.layout.activity_main)
         //changed
 
     }
